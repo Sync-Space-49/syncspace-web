0984d6d7a7d074fcd55ce3c46b59c1f5520ba4c2
MJDorcelien <lydia.dorcelien@gmail.com>
2023-10-19 14:12:02 -0400

    kinda added profile....ish

--- src/Profile/profile.js
+++ src/Profile/profile.js
@@ -2,8 +2,17 @@ import React from "react";
 
 function Profile() {
   return (
-    <div>
-        <p>Profile</p>
+    <div className="flex align-top">
+        <div className="flex bg-blue h-24 w-full">        
+          <div>
+            <div className="rounded-md flex-row bg-success px-3 py-2 text-sm font-medium">
+              <p>Profile</p>
+            </div>
+            <div className="rounded-md flex-row bg-success px-3 py-2 text-sm font-medium">
+              <p>Settings</p>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
