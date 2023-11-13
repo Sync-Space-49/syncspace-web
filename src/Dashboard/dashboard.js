e14c858ecba36649b2ccc25e3e0117618abdae55
MJDorcelien <lydia.dorcelien@gmail.com>
2023-11-13 18:10:33 -0500

    added auth0

--- src/Dashboard/dashboard.js
+++ src/Dashboard/dashboard.js
@@ -1,9 +1,51 @@
+// import { useAuth0 } from "@auth0/auth0-react";
+// import React from "react";
+
+// const DashboardLogIn = () => {
+//   const { loginWithRedirect } = useAuth0();
+
+//   return <button className="bg-primary w-24 rounded" onClick={() => loginWithRedirect()}>Log In</button>;
+// };
+
+// export default DashboardLogIn;
+
 import React from "react";
 
 function Dashboard() {
   return (
     <div>
         <p>Dashboard</p>
+        
+        <div className="bg-light w-24 rounded">
+          <p> Log In</p>
+        </div>
+        <div className="bg-dark w-24 rounded">
+          <p> Log In</p>
+        </div>
+        <div className="bg-primary w-24 rounded">
+          <p> Log In</p>
+        </div>
+        <div className="bg-secondary w-24 rounded">
+          <p> Log In</p>
+        </div>
+        <div className="bg-tertiary w-24 rounded">
+          <p> Log In</p>
+        </div>
+        <div className="bg-blue w-24 rounded">
+          <p> Log In</p>
+        </div>
+        <div className="bg-black w-24 rounded">
+          <p> Log In</p>
+        </div>
+        <div className="bg-success w-24 rounded">
+          <p> Log In</p>
+        </div>
+        <div className="bg-info w-24 rounded">
+          <p> Log In</p>
+        </div>
+        <div className="bg-danger w-24 rounded">
+          <p> Log In</p>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,34 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const root = createRoot(document.getElementById('root'));
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_FRONTEND_CLIENT_ID;
+const direct = process.env.REACT_APP_REDIRECT_URI
+
 root.render(
-  <React.StrictMode>
+<Auth0Provider
+    domain = {domain}
+    clientId = {clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
