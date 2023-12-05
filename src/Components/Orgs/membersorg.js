1c782c9e69f291598877ff8899cefceecc6ebd5b
MJDorcelien <lydia.dorcelien@gmail.com>
2023-12-05 14:42:26 -0500

    implemented add/remove members from org

--- src/Components/Orgs/membersorg.js
+++ src/Components/Orgs/membersorg.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useRemoveMemberOrg } from "../../hooks/Orgs/useRemoveMemberOrg";
 
 const MembersOrg = ({member}) => {
+  const { removeMemberOrg } = useRemoveMemberOrg()
+
+  const onRemoveMember = (e) => {
+    e.preventDefault()
+    removeMemberOrg({
+      userID: member.user_id
+    })
+  }
   return (
     <div>
-        <p>{member.username} is a member of this organization</p>
+      <p>{member.username} is a member of this organization
+        <button 
+          className="text-white font-semibold rounded-md m-4 p-2 bg-danger"
+          onClick={ onRemoveMember }
+          >
+          Leave Organization
+        </button>
+      </p>
     </div>
   );
 };
